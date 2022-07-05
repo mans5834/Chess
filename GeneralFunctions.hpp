e3028ccb5c240ae861a30334d094c69b079d3ed3
mans5834 <105821977+mans5834@users.noreply.github.com>
2022-07-04 22:10:44 -0700

    Update GeneralFunctions.hpp

--- GeneralFunctions.hpp
+++ GeneralFunctions.hpp
@@ -1,1 +1,21 @@
+#include <iostream>
 
+using namespace std;
+
+//start of function declarations
+void ChessBoard();
+//end of function declarations
+
+void ChessBoard(){
+  for(int z = 0; z < 8; z++){
+    for(int y = 0; y < 49; y++){
+      cout << "-";
+    }
+    for(int y = 0; y < 8; y++){
+      cout << "|     |     |     |     |     |     |     |     |" << endl;
+    }
+  }
+  for(int y = 0; y < 49; y++){
+    cout << "-";
+  }
+}
